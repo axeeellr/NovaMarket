2572fb28f9ac7b213e11026c1c3e8ac162687b68
axeeellr <axelramireezz@gmail.com>
2024-03-13 22:31:15 -0600

    login y profile 1.0

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -8,27 +8,29 @@ import { faHome, faStore, faBell, faCog, faQrcode } from '@fortawesome/free-soli
 
 function Menu() {
   return (
-    <div className="menu">
-      <div className="menu__left">
-        <Link to="/"> {/* Agrega un enlace al ícono de inicio */}
-          <FontAwesomeIcon icon={faHome} />
-        </Link>
-        <Link to="/shop"> {/* Agrega un enlace al ícono de la tienda */}
-          <FontAwesomeIcon icon={faStore} />
-        </Link>
-      </div>
-      <div className="menu__center">
-        <Link to="/scanner"> {/* Agrega un enlace al ícono de la tienda */}
-          <img src={qrCode} className='scannerIcon' />
-        </Link>
-      </div>
-      <div className="menu__right">
-        <Link to="/notifications"> {/* Agrega un enlace al ícono de notificaciones */}
-          <FontAwesomeIcon icon={faBell} />
-        </Link>
-        <Link to="/profile"> {/* Agrega un enlace al ícono de configuración */}
-          <FontAwesomeIcon icon={faCog} />
-        </Link>
+    <div className="menu__container">
+      <div className="menu">
+        <div className="menu__left">
+          <Link to="/"> {/* Agrega un enlace al ícono de inicio */}
+            <FontAwesomeIcon icon={faHome} />
+          </Link>
+          <Link to="/shop"> {/* Agrega un enlace al ícono de la tienda */}
+            <FontAwesomeIcon icon={faStore} />
+          </Link>
+        </div>
+        <div className="menu__center">
+          <Link to="/scanner"> {/* Agrega un enlace al ícono de la tienda */}
+            <img src={qrCode} className='scannerIcon' />
+          </Link>
+        </div>
+        <div className="menu__right">
+          <Link to="/notifications"> {/* Agrega un enlace al ícono de notificaciones */}
+            <FontAwesomeIcon icon={faBell} />
+          </Link>
+          <Link to="/profile"> {/* Agrega un enlace al ícono de configuración */}
+            <FontAwesomeIcon icon={faCog} />
+          </Link>
+        </div>
       </div>
     </div>
   );
--- src/views/Login.jsx
+++ src/views/Login.jsx
@@ -2,31 +2,49 @@ import React from 'react'
 import '../css/root.css'
 import '../css/login.css'
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGoogle, faFacebook, faTwitter } from '@fortawesome/free-brands-svg-icons';
+
 function Login() {
     return (
-        <div className="body">
-            <div className="main">  	
-                <input type="checkbox" id="chk" aria-hidden="true" />
-                <div className="signup">
-                    <form>
-                        <label htmlFor="chk" aria-hidden="true">Registro</label>
+        <div className="main">  	
+            <input type="checkbox" id="chk" aria-hidden="true" />
+            <div className="signup">
+                <form>
+                    <label htmlFor="chk" aria-hidden="true">Registro</label>
+                    <div className="inputs">
                         <input type="text" name="txt" placeholder="Nombre" required />
                         <input type="email" name="email" placeholder="Correo electrónico" required />
                         <input type="password" name="pswd" placeholder="Contraseña" required />
                         <button>Entrar</button>
-                    </form>
-                </div>
-                <div className="login">
-                    <form>
-                        <label htmlFor="chk" aria-hidden="true">Iniciar sesión</label>
+                    </div>
+                    <p className='socialTextSign'>También puedes usar...</p>
+                    <div className="social">
+                        <FontAwesomeIcon icon={faGoogle} />
+                        <FontAwesomeIcon icon={faFacebook} />
+                        <FontAwesomeIcon icon={faTwitter} />
+                    </div>
+                </form>
+            </div>
+            <div className="login">
+                <form>
+                    <label htmlFor="chk" aria-hidden="true">Iniciar Sesión</label>
+                    <div className="inputs">
                         <input type="email" name="email" placeholder="Correo electrónico" required />
                         <input type="password" name="pswd" placeholder="Contraseña" required />
                         <button>Entrar</button>
-                    </form>
-                </div>
+                    </div>
+                    <p className='socialTextLogin'>También puedes usar...</p>
+                    <div className="social">
+                        <FontAwesomeIcon icon={faGoogle} />
+                        <FontAwesomeIcon icon={faFacebook} />
+                        <FontAwesomeIcon icon={faTwitter} />
+                    </div>
+                </form>
             </div>
         </div>
 	);
 }
 
 export default Login;
+
