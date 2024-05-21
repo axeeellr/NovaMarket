91512a47c0faa3077f975a8b680ddd36ff31f608
axeeellr <axelramireezz@gmail.com>
2024-05-20 22:40:46 -0600

    circulos para shop

--- frontend/src/views/Fruits.jsx
+++ frontend/src/views/Fruits.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 import ImageMapper from 'react-image-mapper';
-import { isMobile } from 'react-device-detect'; // Importa la función isMobile
+import { isMobile } from 'react-device-detect';
+import { useNavigate } from 'react-router-dom';
+
 import TitlePage from '../components/TitlePage';
 import MenuShop from '../components/MenuShop';
 
@@ -8,41 +10,40 @@ import fruits from '../assets/fruits.jpg';
 import '../css/shop.css';
 
 const Fruits = () => {
+    const navigate = useNavigate();
     const [menuVisible, setMenuVisible] = useState(false);
+    const [hoveredArea, setHoveredArea] = useState(null);
 
     const toggleMenuVisibility = () => {
         setMenuVisible(!menuVisible);
     };
 
-    // Define las coordenadas para PC y móvil
     const PC_COORDS = [
-        // Coordenadas para PC
-        { name: 'Mandarinas', shape: 'circle', coords: [130, 540, 10], preFillColor: 'rgba(255, 255, 255, 0.7)', strokeColor: 'black', lineWidth: 5},
-        { name: 'Peras', shape: 'circle', coords: [300, 450, 10], preFillColor: 'rgba(255, 255, 255, 0.7)', strokeColor: 'black', lineWidth: 5 },
-        { name: 'Tomates', shape: 'circle', coords: [100, 360, 10], preFillColor: 'rgba(255, 255, 255, 0.7)', strokeColor: 'black', lineWidth: 5 },
-        { name: 'Uvas', shape: 'circle', coords: [290, 345, 10], preFillColor: 'rgba(255, 255, 255, 0.7)', strokeColor: 'black', lineWidth: 5 },
-        { name: 'Manzanas', shape: 'circle', coords: [450, 400, 10], preFillColor: 'rgba(255, 255, 255, 0.7)', strokeColor: 'black', lineWidth: 5 },
-        { name: 'Naranjas', shape: 'circle', coords: [510, 340, 10], preFillColor: 'rgba(255, 255, 255, 0.7)', strokeColor: 'black', lineWidth: 5 },
-        { name: 'Mangos', shape: 'circle', coords: [1400, 450, 10], preFillColor: 'rgba(255, 255, 255, 0.7)', strokeColor: 'black', lineWidth: 5 },
-        { name: 'Rábanos', shape: 'circle', coords: [1270, 400, 10], preFillColor: 'rgba(255, 255, 255, 0.7)', strokeColor: 'black', lineWidth: 5 },
-        { name: 'Cilantro', shape: 'circle', coords: [1190, 380, 10], preFillColor: 'rgba(255, 255, 255, 0.7)', strokeColor: 'black', lineWidth: 5 },
-        { name: 'Pepinos', shape: 'circle', coords: [1280, 310, 10], preFillColor: 'rgba(255, 255, 255, 0.7)', strokeColor: 'black', lineWidth: 5 },
-        { name: 'Papas', shape: 'circle', coords: [1120, 365, 10], preFillColor: 'rgba(255, 255, 255, 0.7)', strokeColor: 'black', lineWidth: 5 },
+        { name: 'Mandarinas', id: 'nm-x-0', shape: 'circle', coords: [130, 540, 10], preFillColor: 'rgba(255, 255, 255, 0.7)', strokeColor: 'black', lineWidth: 5},
+        { name: 'Peras', id: 'nm-x-0', shape: 'circle', coords: [300, 450, 10], preFillColor: 'rgba(255, 255, 255, 0.7)', strokeColor: 'black', lineWidth: 5 },
+        { name: 'Tomates', id: 'nm-x-0', shape: 'circle', coords: [100, 360, 10], preFillColor: 'rgba(255, 255, 255, 0.7)', strokeColor: 'black', lineWidth: 5 },
+        { name: 'Uvas', id: 'nm-x-0', shape: 'circle', coords: [290, 345, 10], preFillColor: 'rgba(255, 255, 255, 0.7)', strokeColor: 'black', lineWidth: 5 },
+        { name: 'Manzanas', id: 'nm-x-0', shape: 'circle', coords: [450, 400, 10], preFillColor: 'rgba(255, 255, 255, 0.7)', strokeColor: 'black', lineWidth: 5 },
+        { name: 'Naranjas', id: 'nm-x-0', shape: 'circle', coords: [510, 340, 10], preFillColor: 'rgba(255, 255, 255, 0.7)', strokeColor: 'black', lineWidth: 5 },
+        { name: 'Mangos', id: 'nm-x-0', shape: 'circle', coords: [1440, 450, 10], preFillColor: 'rgba(255, 255, 255, 0.7)', strokeColor: 'black', lineWidth: 5 },
+        { name: 'Rábanos', id: 'nm-x-0', shape: 'circle', coords: [1320, 400, 10], preFillColor: 'rgba(255, 255, 255, 0.7)', strokeColor: 'black', lineWidth: 5 },
+        { name: 'Cilantro', id: 'nm-x-0', shape: 'circle', coords: [1230, 380, 10], preFillColor: 'rgba(255, 255, 255, 0.7)', strokeColor: 'black', lineWidth: 5 },
+        { name: 'Pepinos', id: 'nm-x-0', shape: 'circle', coords: [1330, 310, 10], preFillColor: 'rgba(255, 255, 255, 0.7)', strokeColor: 'black', lineWidth: 5 },
+        { name: 'Papas', id: 'nm-x-0', shape: 'circle', coords: [1170, 365, 10], preFillColor: 'rgba(255, 255, 255, 0.7)', strokeColor: 'black', lineWidth: 5 },
     ];
 
     const MOBILE_COORDS = [
-        // Coordenadas para dispositivos móviles
-        { name: 'Mandarinas', shape: 'circle', coords: [130, 600, 10], preFillColor: 'rgba(255, 255, 255, 0.7)', strokeColor: 'black', lineWidth: 5},
-        { name: 'Peras', shape: 'circle', coords: [300, 530, 10], preFillColor: 'rgba(255, 255, 255, 0.7)', strokeColor: 'black', lineWidth: 5 },
-        { name: 'Tomates', shape: 'circle', coords: [100, 430, 10], preFillColor: 'rgba(255, 255, 255, 0.7)', strokeColor: 'black', lineWidth: 5 },
-        { name: 'Uvas', shape: 'circle', coords: [290, 415, 10], preFillColor: 'rgba(255, 255, 255, 0.7)', strokeColor: 'black', lineWidth: 5 },
-        { name: 'Manzanas', shape: 'circle', coords: [450, 480, 10], preFillColor: 'rgba(255, 255, 255, 0.7)', strokeColor: 'black', lineWidth: 5 },
-        { name: 'Naranjas', shape: 'circle', coords: [510, 400, 10], preFillColor: 'rgba(255, 255, 255, 0.7)', strokeColor: 'black', lineWidth: 5 },
-        { name: 'Mangos', shape: 'circle', coords: [1450, 510, 10], preFillColor: 'rgba(255, 255, 255, 0.7)', strokeColor: 'black', lineWidth: 5 },
-        { name: 'Rábanos', shape: 'circle', coords: [1350, 455, 10], preFillColor: 'rgba(255, 255, 255, 0.7)', strokeColor: 'black', lineWidth: 5 },
-        { name: 'Cilantro', shape: 'circle', coords: [1250, 440, 10], preFillColor: 'rgba(255, 255, 255, 0.7)', strokeColor: 'black', lineWidth: 5 },
-        { name: 'Pepinos', shape: 'circle', coords: [1340, 380, 10], preFillColor: 'rgba(255, 255, 255, 0.7)', strokeColor: 'black', lineWidth: 5 },
-        { name: 'Papas', shape: 'circle', coords: [1240, 380, 10], preFillColor: 'rgba(255, 255, 255, 0.7)', strokeColor: 'black', lineWidth: 5 },
+        { name: 'Mandarinas', id: 'nm-x-0', shape: 'circle', coords: [130, 600, 10], preFillColor: 'rgba(255, 255, 255, 0.7)', strokeColor: 'black', lineWidth: 5},
+        { name: 'Peras', id: 'nm-x-0', shape: 'circle', coords: [300, 530, 10], preFillColor: 'rgba(255, 255, 255, 0.7)', strokeColor: 'black', lineWidth: 5 },
+        { name: 'Tomates', id: 'nm-x-0', shape: 'circle', coords: [100, 430, 10], preFillColor: 'rgba(255, 255, 255, 0.7)', strokeColor: 'black', lineWidth: 5 },
+        { name: 'Uvas', id: 'nm-x-0', shape: 'circle', coords: [290, 415, 10], preFillColor: 'rgba(255, 255, 255, 0.7)', strokeColor: 'black', lineWidth: 5 },
+        { name: 'Manzanas', id: 'nm-x-0', shape: 'circle', coords: [450, 480, 10], preFillColor: 'rgba(255, 255, 255, 0.7)', strokeColor: 'black', lineWidth: 5 },
+        { name: 'Naranjas', id: 'nm-x-0', shape: 'circle', coords: [510, 400, 10], preFillColor: 'rgba(255, 255, 255, 0.7)', strokeColor: 'black', lineWidth: 5 },
+        { name: 'Mangos', id: 'nm-x-0', shape: 'circle', coords: [1450, 510, 10], preFillColor: 'rgba(255, 255, 255, 0.7)', strokeColor: 'black', lineWidth: 5 },
+        { name: 'Rábanos', id: 'nm-x-0', shape: 'circle', coords: [1350, 455, 10], preFillColor: 'rgba(255, 255, 255, 0.7)', strokeColor: 'black', lineWidth: 5 },
+        { name: 'Cilantro', id: 'nm-x-0', shape: 'circle', coords: [1250, 440, 10], preFillColor: 'rgba(255, 255, 255, 0.7)', strokeColor: 'black', lineWidth: 5 },
+        { name: 'Pepinos', id: 'nm-x-0', shape: 'circle', coords: [1340, 380, 10], preFillColor: 'rgba(255, 255, 255, 0.7)', strokeColor: 'black', lineWidth: 5 },
+        { name: 'Papas', id: 'nm-x-0', shape: 'circle', coords: [1240, 380, 10], preFillColor: 'rgba(255, 255, 255, 0.7)', strokeColor: 'black', lineWidth: 5 },
     ];
 
     // Utiliza las coordenadas adecuadas según el tipo de dispositivo
@@ -52,21 +53,55 @@ const Fruits = () => {
     };
 
     const handleClick = area => {
-        console.log('Area clicked:', area);
+        navigate(`/product/${area.id}`);
+    };
+
+    const handleMouseEnter = area => {
+        setHoveredArea(area);
+    };
+
+    const handleMouseLeave = () => {
+        setHoveredArea(null);
     };
 
     return(
         <>
             <TitlePage />
             <div className="shop__container">
-                <ImageMapper src={fruits} map={MAP} onClick={handleClick} className="container__img"/>
+                <ImageMapper 
+                    src={fruits} 
+                    map={MAP} 
+                    onClick={handleClick} 
+                    onMouseEnter={handleMouseEnter} 
+                    onMouseLeave={handleMouseLeave}  
+                    className="container__img"
+                />
                 <MenuShop menuVisible={menuVisible} toggleMenuVisibility={toggleMenuVisibility} />
                 <div className="shop__sections__button">
                     <button onClick={toggleMenuVisibility}>Pasillos</button>
                 </div>
+                {hoveredArea && (
+                    <div 
+                        className="hover__label" 
+                        style={{
+                            position: 'absolute',
+                            left: `${hoveredArea.coords[0]}px`,
+                            top: `${hoveredArea.coords[1] + 20}px`, // Ajusta la posición para estar justo debajo
+                            transform: 'translate(-50%, 0)', // Centrar horizontalmente
+                            background: 'rgba(0, 0, 0, 0.7)',
+                            color: 'white',
+                            padding: '5px',
+                            borderRadius: '5px',
+                            pointerEvents: 'none',
+                            zIndex: 1000,
+                        }}
+                    >
+                        {hoveredArea.name}
+                    </div>
+                )}
             </div>
         </>
     )
 }
 
-export default Fruits;
+export default Fruits
--- frontend/src/views/Shop.jsx
+++ frontend/src/views/Shop.jsx
@@ -1,4 +1,8 @@
 import React, {useState} from 'react';
+import ImageMapper from 'react-image-mapper';
+import { isMobile } from 'react-device-detect';
+import { useNavigate } from 'react-router-dom';
+
 import TitlePage from '../components/TitlePage';
 import MenuShop from '../components/MenuShop';
 import entrance from '../assets/entrancee.jpg';
@@ -6,21 +10,75 @@ import '../css/shop.css';
 
 const Shop = () => {
 
+    const navigate = useNavigate();
     const [menuVisible, setMenuVisible] = useState(false);
+    const [hoveredArea, setHoveredArea] = useState(null);
 
     const toggleMenuVisibility = () => {
         setMenuVisible(!menuVisible);
+    };
+
+    const PC_COORDS = [
+        { name: 'Entrar a NovaMarket', id: 'nm-x-0', shape: 'circle', coords: [820, 470, 10], preFillColor: 'rgba(255, 255, 255, 0.7)', strokeColor: 'black', lineWidth: 5},
+    ];
+
+    const MOBILE_COORDS = [
+        { name: 'Entrar a NovaMarket', id: 'nm-x-0', shape: 'circle', coords: [130, 600, 10], preFillColor: 'rgba(255, 255, 255, 0.7)', strokeColor: 'black', lineWidth: 5},
+    ];
+
+    const MAP = {
+        name: 'my-map',
+        areas: isMobile ? MOBILE_COORDS : PC_COORDS,
+    };
+
+    const handleClick = area => {
+        navigate(`/shop/fruits`);
+    };
+
+    const handleMouseEnter = area => {
+        setHoveredArea(area);
+    };
+
+    const handleMouseLeave = () => {
+        setHoveredArea(null);
     };
 
     return(
         <>
             <TitlePage />
             <div className="shop__container">
-                <img src={entrance}/>
+                <ImageMapper 
+                    src={entrance} 
+                    map={MAP} 
+                    onClick={handleClick}
+                    onMouseEnter={handleMouseEnter}
+                    onMouseLeave={handleMouseLeave}  
+                    className="container__img"
+                />
                 <MenuShop menuVisible={menuVisible} toggleMenuVisibility={toggleMenuVisibility} />
                 <div className="shop__sections__button">
                     <button onClick={toggleMenuVisibility}>Pasillos</button>
                 </div>
+                {hoveredArea && (
+                    <div 
+                        className="hover__label" 
+                        style={{
+                            position: 'absolute',
+                            left: `${hoveredArea.coords[0]}px`,
+                            top: `${hoveredArea.coords[1] + 20}px`, // Ajusta la posición para estar justo debajo
+                            transform: 'translate(-50%, 0)', // Centrar horizontalmente
+                            background: 'rgba(0, 0, 0, 0.7)',
+                            color: 'white',
+                            padding: '5px',
+                            borderRadius: '5px',
+                            pointerEvents: 'none',
+                            zIndex: 1000,
+                            textAlign: 'center'
+                        }}
+                    >
+                        {hoveredArea.name}
+                    </div>
+                )}
             </div>
         </>
     )
