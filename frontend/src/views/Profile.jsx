404afa93438b730e10aee2b5f5736da08ce642d6
axeeellr <axelramireezz@gmail.com>
2024-06-03 10:13:52 -0600

    hola

--- frontend/src/views/Profile.jsx
+++ frontend/src/views/Profile.jsx
@@ -16,6 +16,8 @@ import PaymentMethods from '../components/PaymentMethods';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSignOut, faEdit } from '@fortawesome/free-solid-svg-icons';
 
+import novalogostar from '../assets/novalogostar.png'
+
 function Profile() {
     const navigate = useNavigate();
     const { logout, user } = useUser();
@@ -36,21 +38,24 @@ function Profile() {
             <div className="title__product">
                 <TitlePage />
             </div>
-            <div className="profile">
-                <div className="profile__data">
-                    <h2>Mis datos</h2>
-                    <button onClick={toggleModal}>EDITAR MIS DATOS &nbsp;<FontAwesomeIcon icon={faEdit} /></button>
-                </div>
-                
-                <History user={user} />
+            <div className="profile__container">
+                <div className="profile">
+                    <div className="profile__data">
+                        <h2>Mis datos</h2>
+                        <button onClick={toggleModal}>EDITAR MIS DATOS &nbsp;<FontAwesomeIcon icon={faEdit} /></button>
+                    </div>
+                    
+                    <History user={user} />
 
-                <PaymentMethods user={user} />
+                    <PaymentMethods user={user} />
 
-                <div className="profile__logout">
-                    <h2>Cerrar Sesión</h2>
-                    <button onClick={handleLogout}>CERRAR SESIÓN &nbsp;<FontAwesomeIcon icon={faSignOut} /></button>
+                    <div className="profile__logout">
+                        <h2>Cerrar Sesión</h2>
+                        <button onClick={handleLogout}>CERRAR SESIÓN &nbsp;<FontAwesomeIcon icon={faSignOut} /></button>
+                    </div>
                 </div>
             </div>
+            <img src={novalogostar} className='novalogo' alt=""/>
             <Menu />
 
             <Toaster
