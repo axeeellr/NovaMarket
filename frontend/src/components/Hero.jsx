da8d89b03e2fcf8f074816449482d623eabd9790
estudiante20120161 <estudiante20120161@cdb.edu.sv>
2024-09-05 22:17:07 -0600

    home responsive

--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -5,12 +5,16 @@ import { useUser } from '../UserContext';
 import logo from '../assets/novamarketv3.png';
 import videobackground from '../assets/supermarketvideo.mp4';
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBasketShopping } from '@fortawesome/free-solid-svg-icons';
+
 import '../css/hero.css';
 
 function Hero() {
     const { isAuthenticated } = useUser();
 
     return (
+        <>
         <div className="hero">
             <video src={videobackground} className='background' id='background-video' autoPlay muted loop playsInline></video>
             <div className="hero__info">
@@ -27,6 +31,24 @@ function Hero() {
                 )}
             </div>
         </div>
+
+        <div className="hero__pc">
+            <video src={videobackground} className='background__pc' id='background-video' autoPlay muted loop playsInline></video>
+            <div className="hero__info__pc">
+                <h1>¡Donde puedes comprar sin esperar!</h1>
+                <p>¡Bienvenido a NovaMarket! Donde puedes hacer tus compras del supermercado fácil, rápido y en un par de clicks.</p>
+                {isAuthenticated === true ? (
+                    <Link to="/shop" style={{ textDecoration: 'none' }}>
+                        <button>COMPRAR</button>
+                    </Link>
+                    ) : (
+                    <Link to="/login" style={{ textDecoration: 'none' }}>
+                        <button>INICIAR</button>
+                    </Link>
+                )}
+            </div>
+        </div>
+        </>
     );
 }
 
--- frontend/src/components/IndexContent.jsx
+++ frontend/src/components/IndexContent.jsx
@@ -2,8 +2,13 @@ import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import '../css/content.css';
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFacebook, faInstagram, faLinkedin } from '@fortawesome/free-brands-svg-icons';
+import { faCircleInfo, faCaretUp } from '@fortawesome/free-solid-svg-icons'; // Añadir iconos
+
 function IndexContent() {
     const [currentStep, setCurrentStep] = useState(0); // Comienza en el paso 0
+    const [isFooterVisible, setFooterVisible] = useState(false); // Estado para controlar visibilidad del footer
 
     const steps = [
         { title: "Escanea los productos", description: "Escanea el código QR de los productos que quieres comprar" },
@@ -17,6 +22,10 @@ function IndexContent() {
         setCurrentStep(index);
     };
 
+    const toggleFooter = () => {
+        setFooterVisible(!isFooterVisible); // Alterna la visibilidad del footer
+    };
+
     return (
         <div className="content">
             <div className="content__infoScanner">
@@ -24,7 +33,7 @@ function IndexContent() {
                     <img src="https://storage.googleapis.com/support-kms-prod/mQmcrC93Ryi2U4x5UdZNeyHQMybbyk71yCVm" alt="Escanea tus productos" />
                 </div>
                 <div className="content__info">
-                    <h3>Empieza a escanear</h3>
+                    <h3>¡Empieza a escanear!</h3>
                     <p>Añade tus productos al carrito escaneando el código QR de cada uno.</p>
                     <Link to="/scanner" style={{ textDecoration: 'none' }}>
                         <button>ESCANEAR</button>
@@ -32,7 +41,7 @@ function IndexContent() {
                 </div>
             </div>
             <div className="content__infoStep">
-                <h3>¿Cómo comprar?</h3>
+                <h2>¿Cómo comprar?</h2>
                 <ul className="StepProgress">
                     {steps.map((step, index) => (
                         <li 
@@ -49,6 +58,42 @@ function IndexContent() {
                     ))}
                 </ul>
             </div>
+
+            <button onClick={toggleFooter} className="toggle-footer-button">
+                <FontAwesomeIcon icon={isFooterVisible ? faCaretUp : faCircleInfo} />
+            </button>
+
+            <footer className={isFooterVisible ? 'show' : ''}>
+                <div className="footer__top">
+                    <div className="footer__about">
+                        <h2>Sobre Nosotros</h2>
+                        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nisi eveniet eaque animi, cupiditate totam et fugit, expedita rem, debitis tempore reiciendis ullam optio? Consequuntur voluptatibus ipsum accusantium veritatis laudantium iste est eius? Ullam provident iusto, minima temporibus.</p>
+                    </div>
+                    <div className="footer__categories">
+                        <h2>Integrantes</h2>
+                        <p>Abner Navarro</p>
+                        <p>Jasmín Flores</p>
+                        <p>Axel Ramírez</p>
+                        <p>René Cornejo</p>
+                    </div>
+                    <div className="footer__links">
+                        <h2>Enlaces</h2>
+                        <p>Acerda de</p>
+                        <p>Contáctanos</p>
+                        <p>Política de privacidad</p>
+                    </div>
+                </div>
+                <div className="footer__bottom">
+                    <div className="footer__copy">
+                        <p>Copyright &copy; 2024 Todos los derechos reservados por NovaMarket</p>
+                    </div>
+                    <div className="footer__media">
+                        <FontAwesomeIcon icon={faFacebook} />
+                        <FontAwesomeIcon icon={faInstagram} />
+                        <FontAwesomeIcon icon={faLinkedin} />
+                    </div>
+                </div>
+            </footer>
         </div>
     );
 }
